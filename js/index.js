b422a6a86673f01b43429cb7a006431e69a80c4b
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-05-26 21:38:26 +0100

    Declare the game board module

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
-const gameBoard = {};
-const players = {};
-const gameFlow = {};
+const GameBoard = (function () {
+  const gameBoard = new Array(9).fill(null);
+  return {
+    gameBoard
+  }
+})();
+
+const PlayerFactory = function (name) {
+  return {
+    name: name
+  }
+};
+
+const GameFlow = (function () {})();
