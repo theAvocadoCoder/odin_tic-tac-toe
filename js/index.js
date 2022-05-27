e7a0928b8633c6356ce2775d59b22b7f65e95128
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-05-27 15:41:22 +0100

    Add player functionality

--- js/index.js
+++ js/index.js
@@ -1,20 +1,39 @@
 const Board = (function () {
+  const setCell = (id, mark) => {
+    board[id] = mark;
+  }
+  const getBoard = () => board;
   // const gameBoard = new Array(9).fill(null);
   const board = ['X', 'X', 'O', 'O', 'O', 'X', 'X', 'O', 'X']
   return {
-    board
+    board: getBoard(),
+    setCell,
   }
 })();
 
-const PlayerFactory = function (name) {
+const PlayerFactory = function (name, mark) {
+  const getName = () => name;
+
+  const getMark = () => mark.toUpperCase();
+
+  const makeMove = (e) => {
+    if (!e.target.firstChild) {
+      e.target.appendChild(document.createTextNode(`getMark()`));
+      const id = e.target.id.split('').pop(); // This selects the very last character in the string
+      Board.setCell(id, getMark());
+    }
+  }
+
   return {
-    name: name
+    name: getName(),
+    mark: getMark(),
+    makeMove,
   }
 };
 
 const Game = (function () {
   const start = function (board) {
-    for (let i = 0; i < board.length; i ++) {
+    for (let i = 0; i < board.length; i++) {
       document.querySelector(`#cell-${i + 1}`).textContent = board[i];
     }
   }
@@ -23,4 +42,4 @@ const Game = (function () {
   }
 })();
 
-window.onload = () => {Game.start(Board.gameBoard)};
+window.onload = () => { Game.start(Board.board) };
